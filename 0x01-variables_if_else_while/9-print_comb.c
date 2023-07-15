47220d43ea3a6235a6e1ac202f649cb26e6cb7bd
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-15 05:50:47 +0300

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: print 0, 1, -9
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar (digit + 48);
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
