e5b030e3c369b918095fbbd22d3ac14b9e4b1c6a
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-15 05:23:52 +0300

    print 0:9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: print all numbers from 0-10
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
