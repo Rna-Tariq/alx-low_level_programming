abf78a3696437b3789bd29bedc776c8ec3895f82
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-15 05:05:48 +0300

    print lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: print alphabet in lowercase then in uppercase
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char = 'a';
+	char_upper = 'A';
+	/* prints a-z */
+	while (char = 'a')
+	{
+		putchar(char);
+		char++;
+	}
+
+	/* print A-Z */
+	while (char_upper <= z)
+	{
+		putchar(char_upper);
+		char_upper++;
+	}
+	putchar('\n');
+	return (0);
+}
