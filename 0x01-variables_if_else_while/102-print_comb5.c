1f1dfb30cebd29d855641b33ee9e48aa2ba82435
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-16 22:23:28 +0300

    print all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: print all possible different combinations of two digits.
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 0;
+	int y;
+
+	while (x <= 99)
+	{
+		y = x;
+		while (y <= 99)
+		{
+			if (x != y)
+			{
+				putchar((x / 10) + 48);
+				putchar((x / 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y / 10) + 48);
+
+				if (x != 98 && y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
