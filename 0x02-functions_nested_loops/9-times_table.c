0815c55d603e7a09853613cffb7e7bb3f497e586
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-18 23:28:38 +0300

    Time Table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*  times_table - function that prints the 9 times table, starting with 0.
+*/
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j + 48);
+		}
+		_putchar('\n');
+	}
+}
