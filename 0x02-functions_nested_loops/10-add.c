50cfd62ed05a5b44845e89c72da7c7d60aa385b0
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-18 23:41:58 +0300

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* add - function that adds two integers and returns the result.
+*
+* @a: first input.
+* @b: second input.
+*
+* Return: result of addition
+*/
+
+int add(int a, int b)
+{
+	c = a + b;
+	return (c);
+}
