92b5626c9a34898eb65afff4de733ce02bda8475
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-19 00:01:19 +0300

    print all from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			printf("%d, ", count);
+			printf("%d, ", i);
 		}
 	}
 	else
 	{
 		for (i = n; i < 98; i--)
 		{
-			printf("%d, ", count);
+			printf("%d, ", i);
 		}
 	}
 	printf("98 \n");
