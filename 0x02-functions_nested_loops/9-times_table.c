e23ce3c34f2856fb6d3090424116312c0341619d
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-18 23:36:02 +0300

    Time Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,26 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
 		{
-			_putchar(i * j + 48);
+			_putchar(',');
+			_putchar(' ');
+			k = i * j;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((k / 10) + 48);
+			}
+			-putchar ((k % 10) + 48);
 		}
 		_putchar('\n');
 	}
