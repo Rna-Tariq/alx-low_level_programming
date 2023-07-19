ecca2db570d6fb5a72b8de6ae988d2d1d223a50f
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-19 17:09:47 +0300

    Times table x15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,13 +27,13 @@ void print_times_table(int n)
 				}
 				else if (prod < 100)
 				{
-					_putchar((prod \ (100)) + (48));
-					_putchar(((prod \ (10)) % (10)) + (48));
+					_putchar((prod / 100) + 48);
+					_putchar(((prod / 10) % 10) + 48);
 				}
 				else
 				{
-					_putchar((prod \ 100) + 48);
-					_putchar(((prod \ 10) % 10) + 48);
+					_putchar((prod / 100) + 48);
+					_putchar(((prod / 10) % 10) + 48);
 					_putchar((prod % 10) + 48);
 				}
 			}
