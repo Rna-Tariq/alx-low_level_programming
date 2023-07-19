07b66f22bd1bb65daa2c0a56b6e8ff5e02a2e341
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-20 00:10:22 +0300

    Fibonacci 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+*  main - Entry point
+*
+* Description: that prints the first 50 Fibonacci numbers,
+* starting with 1 and 2, followed by a new line.
+*
+* Return: 0 Success
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (i = 0; i <= 50; i++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (i == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+		return (0);
+	}
+}
