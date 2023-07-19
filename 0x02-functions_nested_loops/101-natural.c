e6b375dedad8b64c9274ada97c1abf5d5dbe604f
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-19 18:12:26 +0300

    Sum of multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,16 +9,17 @@
 * Return: 0 Success
 */
 
-int main(void) {
-    int i, sum;
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
-    printf("%d\n", sum);
+int main(void)
+{
+	int i, sum;
+		for (i = 0; i < 1024; i++)
+		{
+			if (i % 3 == 0 || i % 5 == 0)
+			{
+				sum += i;
+			}
+		}
+	printf("%d\n", sum);
 
-    return (0);
+	return (0);
 }
