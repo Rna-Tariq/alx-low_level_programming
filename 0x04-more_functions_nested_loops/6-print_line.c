6ca5fc43c9fc6b4e989a4b72cb94405ce47dd2d4
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-20 21:02:13 +0300

    Print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_line - function  draws a straight line in the terminal.
+*
+* @n: input
+*
+* Return: void
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
