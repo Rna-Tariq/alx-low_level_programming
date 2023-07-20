5718dcd1d355fcedf0fd385ef994a0d970bc1629
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-20 17:39:51 +0300

    print from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,7 @@
 * print_numbers - function that prints the numbers,
 * from 0 to 9, followed by a new line.
 *
-* Return: void
+* Return: Always (0) Success.
 */
 
 void print_numbers(void)
@@ -14,8 +14,6 @@ void print_numbers(void)
 	do {
 		_putchar(i + 48);
 		i++;
-	} while (i >= 0  && i <= 9)
-	{
-		_putchar('\n');
-	}
+	} while (i >= 0  && i <= 9);
+	_putchar('\n');
 }
