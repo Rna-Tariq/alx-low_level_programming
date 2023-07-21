b6ed1714528d703bbecc005ae221715a2a691825
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-21 20:06:13 +0300

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: (0) Success.
+*/
+
+int main(void)
+{
+	int n, i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ")
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz "
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
