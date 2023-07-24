056127811c9281c018587265900cec83ba856813
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-24 18:13:17 +0300

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (i < n)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+	else
 	{
 		printf("%d", a[i]);
 	}
