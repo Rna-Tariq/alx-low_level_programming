a55b7f4a416734924d20cae54fd910c98c7715fa
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-24 22:02:27 +0300

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* *_strcpy - a function that copies the string pointed to by src,
+*  including the terminating null byte (\0), to the buffer pointed to by dest.
+*
+* @dest: first integer.
+* @src: second integer.
+*
+* Return: the pointer to dest.
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	while (i != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
