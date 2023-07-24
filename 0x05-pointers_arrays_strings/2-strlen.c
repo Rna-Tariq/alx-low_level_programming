d7b9ed0b6027a2f7e1ffcd743a664f51632f5a44
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-24 14:37:00 +0300

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - a function that returns the length of a string.
+*
+* @s: input.
+*
+* Return: length of a string.
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		i++
+	}
+	return (i);
+}
