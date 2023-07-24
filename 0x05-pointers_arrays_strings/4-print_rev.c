4470f1647c10b2dc43cdf0fcbe75aa1563b888dd
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-24 14:58:05 +0300

    Print on reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,10 +13,13 @@ void print_rev(char *s)
 {
 	int i, len, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		len++;
+		i++;
 	}
+	len = i
 
 	for (j = len - 1; j >= 0; j--)
 	{
