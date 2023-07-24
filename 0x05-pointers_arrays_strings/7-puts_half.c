e77159e3d7d6b2128b76aa99cb520fe7aaf037a4
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-24 15:40:19 +0300

    The half of it

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* puts_half - a function that prints half of a string, followed by a new line.
+*
+* @str: input.
+*
+* Return: void.
+*/
+
+void puts_half(char *str)
+{
+
+	int i, len, j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i - 1;
+
+	for (j = 0; j <= len / 2; j++)
+	{
+	i	_putchar(str[j]);
+	}
+	_putchar('\n');
+}
