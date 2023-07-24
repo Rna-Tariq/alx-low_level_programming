02c243bbe9911963c2184b8bcfb82e09874da59c
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-24 15:23:37 +0300

    1 character at a time

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* puts2 - a function that prints every other character of a string,
+* starting with the first character, followed by a new line.
+*
+* @str: input.
+*
+* Return: void.
+*/
+
+void puts2(char *str)
+{
+	int i, len, j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i - 1;
+
+	for (j = 0; j <= len; j++)
+	{
+		_putchar(str[j]);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
