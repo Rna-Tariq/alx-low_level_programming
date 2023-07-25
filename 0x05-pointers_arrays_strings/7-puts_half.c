4356d2d47e0fd1d1a3de05cb998cf5492cf82d74
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-25 16:13:40 +0300

    Print the Second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,18 +21,9 @@ void puts_half(char *str)
 	}
 	len = i;
 
-	if (len % 2 == 0)
+	for (j = (len - 1) / 2; j < len - 1; j++)
 	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	} else if (len % 2)
-	{
-		for (j = (len - 1) / 2; j < len - 1; j++)
-		{
-			_putchar(str[j + 1]);
-		}
+		_putchar(str[j + 1]);
 	}
 	_putchar('\n');
 }
