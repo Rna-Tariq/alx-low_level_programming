0a9c32210c7bd66ed9435e2272b2dd8ad1e16554
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-27 14:54:53 +0300

    Array reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* reverse_array - a function that reverses the content of an array of integers.
+*
+* @a: first input.
+* @n: second input.
+*
+* Return: void.
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		if (i == 0)
+		{
+			_putchar(a[i]);
+		}
+		else if (i > 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(a[i]);
+		}
+	}
+	_putchar('\n');
+}
