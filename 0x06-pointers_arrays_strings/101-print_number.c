3fe0128f6478f2be5a7ecad3a6c9447bff6338c0
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-27 17:18:56 +0300

    Print a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,23 +10,18 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int digit = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
+		digit = -digit;
 	}
 
-	else
+	if ((digit / 10) > 0)
 	{
-		n1 = n;
+		print_number(digit / 10);
 	}
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+	_putchar((digit % 10) + '0');
 }
