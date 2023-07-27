e5c16a740f1d0d3b1d47bdb048da143a8067c7ac
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-27 15:46:37 +0300

    To uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,18 +11,14 @@
 
 char *string_toupper(char *s)
 {
-	int i, len;
+	int i;
 
-	len = 0;
-
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
-		_putchar(s[i] + 32);
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] + 32;
+		}
 	}
 	return (s);
 }
