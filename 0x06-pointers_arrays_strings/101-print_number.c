5aefcb0b017cf260f27980ecd3b6293aaedcb2d0
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-27 17:08:58 +0300

    Print a number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_number - a function encodes a string into 1337.
+*
+* @n: input.
+*
+* Return: void.
+*/
+
+void print_number(int n)
+{
+	_putchar(n + 48);
+	_putchar('\n');
+}
