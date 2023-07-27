a41f16b2c97aa3fced161f2f924c3f4ce1cba4d3
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-27 14:18:47 +0300

    Copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* *_strncpy - a function that copies a string.
+*
+* @dest: first input.
+* @src: second input.
+* @n: third input.
+*
+* Return: a pointer to the resulting string dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
