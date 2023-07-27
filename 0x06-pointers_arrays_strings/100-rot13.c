3255369c008e11baa5b621fb048cd6d1f99d8ec0
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-27 17:04:55 +0300

    rot13 encoded

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* *rot13 - a function encodes a string into 1337.
+*
+* @s: input.
+*
+* Return: encoded string.
+*/
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
