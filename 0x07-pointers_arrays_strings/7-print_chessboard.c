3e8d9fc643dd60dbcafcca3fceca4a074493ad69
Rna341 <rna.tariq.aboouf@gmail.com>
2023-07-31 22:12:20 +0300

    Chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print_chessboard - prints chessboard.
-* @a: matrix
+* print_chessboard - prints the chessboard
+* @a: two dimension array to print
 */
 
 void print_chessboard(char (*a)[8])
@@ -13,8 +13,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(*(*(i + a) + j));
+			_putchar(a[i][j]);
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
