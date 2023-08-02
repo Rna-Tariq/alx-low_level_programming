46fb91e69acbeccc100fe630f00d9279eea3c983
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-02 14:44:29 +0300

    Recursive print

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _puts_recursion - function that prints a string, followed by a new line.
+*
+* @s: input
+*
+* Return: void.
+*/
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i];
+		_puts_recursion(char *s);
+	}
+	_putchar('\n');
+}
