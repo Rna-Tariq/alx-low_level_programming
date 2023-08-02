2757fc94b6529ba4b7e285b47843e61774f1b55a
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-02 16:07:39 +0300

    Recursive reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,24 +10,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i, len, j;
-
-	len = 0;
-
-	while (s[i] != '\0')
+	if (*s)
 	{
-		len++;
-	}
-
-	i = len - 1;
-
-	if (j <= len)
-	{
-		putchar(s[i]);
-		_print_rev_recursion(s[i--]);
-	}
-	else
-	{
-		putchar('\n');
+		_print_rev_recursion(s + 1);
+		putchar(*s);
 	}
 }
