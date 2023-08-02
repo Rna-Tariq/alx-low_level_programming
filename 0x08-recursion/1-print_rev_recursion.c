3cde3a8b2e22620a51e8d32a76a2b48a9da380fb
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-02 14:57:49 +0300

    Recursive reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - function that prints a string in reverse.
+*
+* @s: input
+*
+* Return: void.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (s[i] == '\0')
+	{
+		putchar(s[i]);
+		_puts_recursion(s - 1);
+	}
+	else if (s[i] != '\0')
+	{
+		putchar('\n');
+	}
+}
