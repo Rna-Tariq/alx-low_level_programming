d01d6dbc3d947d1a5de83038d0755c153cde5356
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-11 17:13:14 +0300

    Concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,11 +26,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
-	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-		;
+	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++);
 
-	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-		;
+	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++);
 
 	str = malloc(s1_len + n + 1);
 
