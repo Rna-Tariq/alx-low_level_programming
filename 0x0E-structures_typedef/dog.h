36b9cc6eec5c3e3dde114303c4c0dd7cdf6c0bae
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-14 16:12:34 +0300

    Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define DOG_H
 
 /**
-* struct dog - a dog struct
+* struct dog - dog data
+* @name: name of the dog
+* @age: age of the dog
+* @owner: name of the owner of the dog
 *
-* @name: dog's name
-* @age: dog's age
-* @owner: owner's name
-*
-* Description: just a lone dog struct in a big kitty world
+* Description: Defines metadata about the dog and type of dog's struct
 */
 
 struct dog
