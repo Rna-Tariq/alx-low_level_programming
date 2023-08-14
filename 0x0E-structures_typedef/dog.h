c103b6c7e8917016adefb54aa928b093f4c3824a
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-14 15:35:07 +0300

    Header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+/**
+* struct dog - a dog struct
+*
+* @name: dog's name
+* @age: dog's age
+* @owner: omner s name
+*
+* Description: just a lone dog struct in a big kitty world
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
