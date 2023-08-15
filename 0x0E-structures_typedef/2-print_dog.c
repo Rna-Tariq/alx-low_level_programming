f9ad9f66e8e3213d11a5fec4a5595a33d629435b
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-15 17:41:08 +0300

    Print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,21 +1,20 @@
-#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "dog.h"
 
 /**
-* print_dog - a function that prints a struct dog
+* print_dog - Prints all the data of a dog
+* @d: A dog structure
 *
-* @d: Pointer refers to the dog struct
-*
-* Return: void
+* Return: Nothing
 */
 
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 		printf("Age: %f\n", (d->age) ? d->age : 0);
-		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
