b6747cae98d396e759e2c302c96609c090805735
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-16 17:58:40 +0300

    Function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+* int_index - a function that searches for an integer.
+*
+* @array: input
+* @size: array size
+* @cmp: function pointer
+*
+* Return: Required integer index
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && siz && cmp)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+			i++
+		}
+	}
+	return (-1);
+}
