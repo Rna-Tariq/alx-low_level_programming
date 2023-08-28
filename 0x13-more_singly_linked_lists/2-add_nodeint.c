a821886d73608c7609cc7f4a3eb227273f6ac771
Rna341 <rna.tariq.aboouf@gmail.com>
2023-08-28 15:05:25 +0300

    Add at the beginning

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* *add_nodeint -  a function that adds a new node at
+* the beginning of a listint_t list.
+*
+* @head: double pointer to the list.
+* @n : nodes' value.
+*
+* Return: the address of the new element,
+* OR NULL if it failed.
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = (*head);
+	(*head) = new;
+	return (*head);
+}
