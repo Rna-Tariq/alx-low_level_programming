1be480c83f6df374d5a34fde2db04677fd2c7b07
Rna341 <rna.tariq.aboouf@gmail.com>
2024-01-25 15:18:58 +0200

    Tested

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,11 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	newNode->n = n;
 	newNode->prev = NULL;
+    newNode->next = *head;
 
 	if (*head != NULL)
 		(*head)->prev = newNode;
 
-	newNode->next = *head;
 	*head = newNode;
 
 	return (newNode);
